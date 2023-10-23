359bf913e29784b2c97ad39e2c18a3b7f0b220d0
Kévin Nové <knove@distech-controls.com>
2023-10-23 10:37:58 -0400

    Add svg in App.

--- src/sandbox/App.tsx
+++ src/sandbox/App.tsx
@@ -1,15 +1,15 @@
 import { Component1 } from '../../dist';
 import { Component2 } from '../../dist/components/component2';
+import ActuatorIcon from '../../dist/icons/actuator.svg';
 
 function App() {
   return (
     <>
       <div />
       <h1>This is a sandbox to test the library.</h1>
-
-      <p>Click on the Vite and React logos to learn more</p>
       <Component1 label="my label" color="blue" />
       <Component2 label="my component2" />
+      <img src={ActuatorIcon} alt="actuator" />
     </>
   );
 }
